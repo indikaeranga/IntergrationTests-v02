7126e666bfc408c92d30c9d5222310cdb5767e21
Indika Eranga <indikaeranga06@gmail.com>
2024-02-22 14:18:14 +0100

    3 integration tests added

--- src/test/java/TestCalculator.java
+++ src/test/java/TestCalculator.java
@@ -74,6 +74,29 @@ public class TestCalculator {
         main=new Main();
         assertEquals(7,main.absoluteValue(7));
     }
+    @Test
+   public void testAdditionAndMultiplication() {
+        main =new Main();
+
+        // Test addition and multiplication together
+        assertEquals(10, main.addition(main.multiply(2, 3), 4));
+    }
+
+    @Test
+    public void testDivisionAndSubtraction() {
+        main =new Main();
+
+        // Test division and subtraction together
+        assertEquals(2, main.division(main.substration(8, 2), 3));
+    }
+
+    @Test
+    public void testPowerAndAbsoluteValue() {
+        main =new Main();
+
+        // Test power and absolute value together
+        assertEquals(25, main.power(main.absoluteValue(-5), 2));
+    }
 
 
 }
