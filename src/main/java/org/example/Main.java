dff8a43cafc042a07d0c1079cac59cc032f184b7
Indika Eranga <indikaeranga06@gmail.com>
2024-02-22 13:57:41 +0100

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hi welcome to calculator");
+
+        }
+
+        public int add(int a ,int b){return a+b;} //add
+        public int sub(int a,int b){return a-b;} //sub
+        public int multi(int a, int b) {return a*b;}  //multi
+    }
